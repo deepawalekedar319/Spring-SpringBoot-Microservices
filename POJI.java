9b0fd178ecb51274bdd3104ba221c70776031f08
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-04-01 18:16:11 +0530

    interface POJI examples

---
+++ POJI.java
@@ -0,0 +1,15 @@
+interface SampleTest1 {
+    // Example 1
+}
+
+interface SampleTest2 extends SampleTestInterface {
+    // Example 2
+}
+
+interface SampleTestInterface {
+    // Example 3
+}
+
+interface SampleTest3 extends javax.servlet.HttpServlet {
+    // Example 4
+}
