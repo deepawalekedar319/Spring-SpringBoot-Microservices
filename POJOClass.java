e9e3deacc751680e2aedefd76afd6f57aa2d477b
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-04-01 18:13:35 +0530

    Examples of POJO classes

---
+++ POJOClass.java
@@ -0,0 +1,26 @@
+class POJOClass1{
+    // POJO class example 1
+}
+
+class POJOClass2 implements java.awt{
+    // POJO class example 2
+}
+
+class POJOClass3 extends TestClass1 {
+    // POJO class example 3
+}
+
+class TestClass1{
+
+}
+ class POJOClass4 implements javax.servlet.Servlets{
+     // not a POJO class example 1
+ }
+
+ class SampleTest{
+     public void getBusinessLogic() {
+         // Hibernate framework logic 
+     }
+     // POJO class example 4 but this class is with exception because it doesnot 
+     // get executed with standard java libraries
+ }
